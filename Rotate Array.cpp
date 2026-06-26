9a706bb87719522202ce85f95c38f71d1ffffc57
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-06-26 12:45:58 +0530

    Update Rotate Array.cpp

--- Rotate Array.cpp
+++ Rotate Array.cpp
@@ -16,19 +16,24 @@ public:
         //     nums[i] = temp[i-(n-k)];
         // }
 
-        // right rotation
-        vector<int> temp(k);
-        // Store last k elements
-        for(int i = 0; i < k; i++){
-            temp[i] = nums[n-k+i];
-        }
-         // Shift remaining elements to the right
-        for(int i = n-k-1; i >= 0; i--){
-            nums[i+k] = nums[i];
-        }
-        // Copy stored elements to front
-        for(int i = 0; i < k; i++){
-            nums[i] = temp[i];
-        }
+        // right rotation tc = o(n) and sp = o(k)
+        // vector<int> temp(k);
+        // // Store last k elements
+        // for(int i = 0; i < k; i++){
+        //     temp[i] = nums[n-k+i];
+        // }
+        //  // Shift remaining elements to the right
+        // for(int i = n-k-1; i >= 0; i--){
+        //     nums[i+k] = nums[i];
+        // }
+        // // Copy stored elements to front
+        // for(int i = 0; i < k; i++){
+        //     nums[i] = temp[i];
+        // }
+
+        //optimal solution 
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
