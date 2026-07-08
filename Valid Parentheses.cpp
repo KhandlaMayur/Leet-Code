11b8f8ab3318541c5ea7a0b124fd93442fd7dd92
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-07-08 12:59:38 +0530

    Update Valid Parentheses.cpp

--- Valid Parentheses.cpp
+++ Valid Parentheses.cpp
@@ -1,28 +1,25 @@
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
     bool isValid(string s) {
+        int n = s.length();
         stack<char> st;
-
-        for(char c : s) {
-            // opening brackets
-            if(c == '(' || c == '{' || c == '[') {
-                st.push(c);
-            }
-            // closing brackets
-            else {
-                if(st.empty()) return false;
-
-                if(c == ')' && st.top() != '(') return false;
-                if(c == '}' && st.top() != '{') return false;
-                if(c == ']' && st.top() != '[') return false;
-
-                st.pop();
+        for(int i = 0; i < n; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+            }else {
+                if(st.size() == 0){
+                    return false;
+                }
+                if((st.top() == '(' && s[i] == ')') || 
+                (st.top() == '{' && s[i] == '}') || 
+                (st.top() == '[' && s[i] == ']') ){
+                    st.pop();
+                }else{
+                    return false;
+                }
             }
         }
-
-        return st.empty();
+        return (st.size() == 0) ? true : false;
+        
     }
 };
