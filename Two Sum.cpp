60074a7a0f9bf41e628975d84e0236649994e6f0
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-07-14 16:06:26 +0530

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -2,39 +2,51 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<pair<int,int>> arr;
+        // vector<pair<int,int>> arr;
 
-        for(int i = 0; i < n; i++){
-            arr.push_back({nums[i], i});
-        }
+        // for(int i = 0; i < n; i++){
+        //     arr.push_back({nums[i], i});
+        // }
 
-        sort(arr.begin(), arr.end());
-        int s = 0;
-        int e = n-1;
+        // sort(arr.begin(), arr.end());
+        // int s = 0;
+        // int e = n-1;
 
-        while(s < e){
-            int sum = arr[s].first + arr[e].first;
+        // while(s < e){
+        //     int sum = arr[s].first + arr[e].first;
 
-            if(sum == target){
-                return {arr[s].second, arr[e].second};
-            }else if(sum < target){
-                s++;
-            }else{
-                e--;
-            }
-        }
-        return {};
+        //     if(sum == target){
+        //         return {arr[s].second, arr[e].second};
+        //     }else if(sum < target){
+        //         s++;
+        //     }else{
+        //         e--;
+        //     }
+        // }
+        // return {};
         // return (istarget) ? {s,e};
 
         //brute force
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i + 1; j < n; j++){
+        //         if(nums[i] + nums[j] == target){
+        //             return {i,j};
+        //         }
+        //     }
+        // }
+        // return{};
+
+        // Optimal approach using hashmap
+        unordered_map<int, int> mp;
         for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(nums[i] + nums[j] == target){
-                    return {i,j};
-                }
+            int first = nums[i];
+            int second = target - first;
+            if(mp.find(second) != mp.end()){
+                return {i, mp[second]};
             }
+            mp[first] = i;
         }
-        return{};
+        return {};
         
     }
 };
