6615d69b37cf76e11b9b13a5af526b7219bd3f5e
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-08-10 16:05:50 +0530

    Create Boats to save People.cpp

---
+++ Boats to save People.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+       int n = people.size(); 
+       sort(people.begin(), people.end());
+       int low = 0;
+       int high = n - 1;
+       int boat = 0;
+       while(low <= high){
+        if(people[low] + people[high] <= limit){
+            low++;
+        }
+        high--;
+        boat++;
+       }
+       return boat;
+    }
+};
