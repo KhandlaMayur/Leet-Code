5a5c724694135dd974c62170a3ed13e6932a99ca
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-08-19 21:40:24 +0530

    Create Linked List Delete at Position(GFG).cpp

---
+++ Linked List/Linked List Delete at Position(GFG).cpp
@@ -0,0 +1,35 @@
+/* Structure of Linked List Node
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    Node* deleteNode(Node* head, int x) {
+        // code here
+        if(head == nullptr){
+            return nullptr;
+        }
+        if(x == 1){
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return head;
+        }
+        Node* temp = head;
+        for(int i = 1; i < x - 1 && temp != nullptr; i++){
+            temp = temp->next;
+        }
+        if(temp->next != nullptr){
+            Node* deletenode = temp->next;
+            temp->next = temp->next->next;
+        }
+        return head;
+    }
+};
